0e35dc8d20a362b8772af23b0f658f52a8152896
fernandezdavid3d <david.feib@gmail.com>
2019-12-05 10:00:14 +0100

    5/12/19

---
+++ Assets/DFI_Scripts/MovementScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public GameObject destino;
+    private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+
+    // Use this for initialization
+    void Start()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+    }
+
+}
